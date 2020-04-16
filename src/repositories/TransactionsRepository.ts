c78c6a582a08c333e62f23c2051b13aa3808dcd1
Daniel Fernando <daniel_fmpc@hotmail.com.br>
2020-04-16 17:39:25 -0300

    Resolvendo os test

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,44 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const initialBalance: Balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
+
+    const balance = this.transactions.reduce((accumulator, currentValue) => {
+      switch (currentValue.type) {
+        case 'income':
+          return {
+            ...accumulator,
+            income: currentValue.value + accumulator.income,
+            total:
+              currentValue.value + accumulator.income - accumulator.outcome,
+          };
+        case 'outcome':
+          return {
+            ...accumulator,
+            outcome: currentValue.value + accumulator.outcome,
+            total:
+              accumulator.income - currentValue.value + accumulator.outcome,
+          };
+        default:
+          return accumulator;
+      }
+    }, initialBalance);
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
